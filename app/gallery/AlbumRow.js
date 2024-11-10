4c988f03f23764d59f8bd1bfeefe8674780d1be9
MHanak1 <michalhanak@duck.com>
2024-11-10 11:57:14 +0100

    added an ability to view an image semi-fullscreen

--- app/gallery/AlbumRow.js
+++ app/gallery/AlbumRow.js
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { POSTS_PER_PAGE, pb } from '@/public/globals.js'
 import { BiSolidImageAlt } from "react-icons/bi";
 
@@ -13,14 +12,14 @@ export default function Gallery_list (album) {
 			<Link className="text-3xl ml-5 sm:ml-10 mt-4 group size-fit" href = {"/gallery/album/" + id}>
 				<strong className="transition-all group-hover:text-accent"> {title} </strong> <span className="dark:text-gray-400 text-gray-600"> {", " + parseInt(date)} </span>
 			</Link>
-			<div className="flex flex-row gap-5 overflow-x-scroll p-5">
+			<div className="flex flex-row gap-5 overflow-x-scroll overflow-y-visible p-5">
 				{
 					images.slice(0, 10).map((image) => {
 						const img_url = pb.files.getUrl(album, image);
 						return(Gallery_image(img_url, image))
 					})
 				}
-				<a className="rounded-xl hover:rounded-none hover:scale-105 transition-all h-[200px] sm:h-[300px] lg:h-[350px] xl:h-[400px] aspect-video shadow-lg shadow-gray-700 dark:shadow-black flex items-center dark:bg-gray-900 bg-gray-200 group" href = {"/gallery/album/" + id}>
+				<a className="rounded-xl hover:rounded-none hover:scale-105 transition-all h-[200px] sm:h-[300px] lg:h-[350px] xl:h-[400px] aspect-[16/10] shadow-lg shadow-gray-700 dark:shadow-black flex items-center dark:bg-gray-900 bg-gray-200 group" href = {"/gallery/album/" + id} key = "open_album">
 					<span className="m-auto text-2xl group-hover:text-accent text-gray-600 transition-all">
 						Open the entire album
 					</span>
@@ -32,16 +31,47 @@ export default function Gallery_list (album) {
 
 function Gallery_image (img_url, img_id) {
 	//const BlurDataURL = await getBase64(img_url)
+	const [showModal, setShowModal] = useState(false);
+
+	const escFunction = useCallback((event) => {
+	if (event.key === "Escape") {
+	  setShowModal(false)
+	}
+	}, []);
+
+	useEffect(() => {
+	document.addEventListener("keydown", escFunction, false);
+
+	return () => {
+	  document.removeEventListener("keydown", escFunction, false);
+	};
+	}, [escFunction]);
 
 	return (
-		<Image
-			src={img_url}
-			width={600}
-			height={400}
-			quality={95}
-			alt={""}
-			key={img_id}
-			className="rounded-xl hover:rounded-none hover:scale-105 transition-all h-[200px] sm:h-[300px] lg:h-[350px] xl:h-[400px] w-max shadow-lg shadow-gray-700 dark:shadow-black"
-		/>
+		<div 
+		className=" h-[200px] sm:h-[300px] lg:h-[350px] xl:h-[400px] aspect-[16/10]" 
+		key = {img_id}>
+			<Image
+				src={img_url}
+				width={600}
+				height={400}
+				quality={90}
+				alt={""}
+				onClick={() => setShowModal(true)}
+				className="rounded-xl hover:rounded-none hover:scale-105 transition-all object-cover size-full shadow-lg shadow-gray-700 dark:shadow-black"
+			/>
+			{showModal && <div className="fixed size-full backdrop-brightness-75 backdrop-blur-sm left-0 top-0 p-2 sm:p-10 " onClick={() => setShowModal(false)}>
+				<Image
+					src={img_url}
+					width={900}
+					height={600}
+					quality={95}
+					alt={""}
+					className="size-full object-contain"
+				/>
+			</div>
+			}
+			
+		</div>
 	)
 }
