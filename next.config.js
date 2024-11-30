df48736b46cc56b2c51bceb2739ec79a7481a27d
MHanak Server <mhanak@mypjuanmc.ddns.net>
2024-11-30 20:37:55 +0100

    messed with the hostnames in the configs

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+//import { pocketbase_url } from './public/globals.js'
+
+module.exports = {
+  images: {
+    domains: [
+      '192.168.1.66',
+      '127.0.0.1',
+      'localhost',
+      'pocketbase.home',
+    ],
+  },
+}
--- public/globals.js
+++ public/globals.js
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
 
-export const pocketbase_url = 'http://192.168.1.66:8090' //set it to wherever your pocketbase is hosted
+export const pocketbase_url = 'http://pocketbase.home:8090'; //set it to wherever your pocketbase is hosted, i use pocketbase.home and then set it to 127.0.0.1 in the hosts file
 export const pb = new PocketBase(pocketbase_url);
 export const POSTS_PER_PAGE = 10;
